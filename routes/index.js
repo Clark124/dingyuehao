4720671bc07334a818e8a4353c322521ee1300fb
Clark124 <53799906@qq.com>
2018-08-16 11:10:23 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var config = {
+  wechat:{
+    appID:"wx20a5345332e70b9e",
+    appSecret:"356aa496a191f22439ee827d330dc51f",
+    token:"miaolegemide"
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const token = config.wechat.token
+  const {signatuire,nonce,timestamp,ecostr} = req.query
+  var str = [token,timestamp,nonce].sort().join("")
+  var sha = sha1(str)
+  if(sha===signatuire){
+    res.send(ecostr)
+  }else{
+    res.send('wrong')
+  }
 });
 
 module.exports = router;
