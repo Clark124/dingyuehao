6ac49895ee7e578cf7f11d21d0e13f2e59d66c9e
Clark124 <53799906@qq.com>
2018-08-16 17:44:05 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var sha1 = require('sha1')
-var Promise = require('bluebird')
-var request = Promise.promisify(require('request'))
+
 var path = require('path')
 var util = require('../libs/util')
 var wechat_file = path.join(__dirname, '../config/wechat.txt')
-
-var prefix = 'https://api.weixin.qq.com/cgi-bin/'
-var api = {
-  accessToken: prefix + 'token?grant_type=client_credential'
-
-}
+var Wechat = require('../wechat/wechat')
 
 var config = {
   wechat: {
@@ -23,66 +17,14 @@ var config = {
     },
     saveAccessToken: function (data) {
       data = JSON.stringify(data)
-      return util.writeFileAsync(wechat_file,data)
+      return util.writeFileAsync(wechat_file, data)
     }
   }
 }
 
-function Wechat(opts) {
-  var that = this
-  this.appID = opts.appID
-  this.appSecret = opts.appSecret
-  this.getAccessToken = opts.getAccessToken
-  this.saveAccessToken = opts.saveAccessToken
-  this.getAccessToken().then(function (data) {
-    try {
-      data = JSON.parse(data)
-    }
-    catch (e) {
-      return that.updateAccessToken(data)
-    }
-    if (that.isValidAccessToken(data)) {
-      Promise.resolve(data)
-    } else {
-      return that.updateAccessToken(data)
-    }
-  }).then(function (data) {
-    that.access_token = data.access_token
-    that.expires_in = data.expires_in
-    that.saveAccessToken(data)
-  })
-}
-
-Wechat.prototype.isValidAccessToken = function (data) {
-  if (!data || !data.access_token || !data.expires_in) {
-    return false
-  }
-  var access_token = data.access_token
-  var expires_in = data.expires_in
-  var now = (new Date().getTime())
-  if (now < expires_in) {
-    return true
-  } else {
-    return false
-  }
-}
-
-Wechat.prototype.updateAccessToken = function (data) {
-  var appID = this.appID
-  var appSecret = this.appSecret
-  var url = api.accessToken + '&appid=' + appID + '&secret=' + appSecret
-  return new Promise(function (resolve, reject) {
-    request({ url: url, json: true }).then(function (res) {
-      var data = res.body
-      var now = (new Date().getTime())
-      var expires_in = now + (data.expires_in - 20) * 1000
-      data.expires_in = expires_in
-      resolve(data)
-    })
-  })
 
 
-}
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -96,7 +38,11 @@ router.get('/', function (req, res, next) {
   } else {
     res.send('wrong')
   }
-  
+
 });
+
+router.post('/',function(req,res){
+  console.log(req.body)
+})
 
 module.exports = router;
